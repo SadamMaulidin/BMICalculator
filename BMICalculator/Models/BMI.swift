d4c81f7a18920a0c95e8ae4c6bd686804ef587d8
SaeMaulidin <sadammaulidin@gmail.com>
2021-03-10 10:24:45 +0700

    Initial Commit

---
+++ BMICalculator/Models/BMI.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+
+struct BMI {
+    
+    // define some variables
+    var value: Float
+    var advice: String
+    var color: UIColor
+    
+}
---
+++ BMICalculator/Models/RoundButton.swift
@@ -0,0 +1,23 @@
+
+import UIKit
+
+@IBDesignable
+open class RoundButton: UIButton {
+ 
+    // define some functions
+    func setup() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = 5.0
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+}
